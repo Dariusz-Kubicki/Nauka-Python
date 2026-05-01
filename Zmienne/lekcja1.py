dcf068720037b5225cabc5b3f58af0dbea76a55d
Dariusz-Kubicki <kubicki.dariusz22@gmail.com>
2026-05-01 16:30:59 +0200

    Formatowanie kodu

--- Zmienne/lekcja1.py
+++ Zmienne/lekcja1.py
@@ -1,4 +1,4 @@
-students_count = 1000 #Liczby całkowite (Integer)
-rating = 4.99 #Liczby zmiennoprzecinkowe/rzeczywiste (Float)
-is_published = True #Wartość logiczna (Boolean)
-course_name = "Python Programming" #Ciąg znaków (String)
+students_count = 1000          # Liczba całkowita (Integer)
+rating = 4.99                  # Liczba zmiennoprzecinkowa (Float)
+is_published = True            # Wartość logiczna (Boolean)
+course_name = "Python Kurs"    # Ciąg znaków (String)
