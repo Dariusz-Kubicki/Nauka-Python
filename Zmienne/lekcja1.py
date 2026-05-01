503d9cfc62200321bbf0a6f17415176f8d2556c9
Dariusz-Kubicki <kubicki.dariusz22@gmail.com>
2026-05-01 13:42:39 +0200

    Dodanie lekcji 1

---
+++ Zmienne/lekcja1.py
@@ -0,0 +1,4 @@
+students_count = 1000 #Liczby całkowite (Integer)
+rating = 4.99 #Liczby zmiennoprzecinkowe/rzeczywiste (Float)
+is_published = True #Wartość logiczna (Boolean)
+course_name = "Python Programming" #Ciąg znaków (String)
