59a77f6fe8e81a8149404bb3b8bd61957581c801
Dariusz-Kubicki <kubicki.dariusz22@gmail.com>
2026-05-02 12:20:57 +0200

    Dodanie lekcji 2
    Add basic string operations and examples in lekcja2.py

---
+++ String/lekcja2.py
@@ -0,0 +1,8 @@
+course = "Python Programming"   # Zmienna string
+print(len(course))              # Wypisuje długość tekstu, czyli ilość znaków w zmiennej course
+print(course[0])                # Wypisuje pierwszy znak zmiennej course, czyli 'P'
+print(course[-1])               # Wypisuje ostatni znak zmiennej course, czyli 'g'
+print(course[0:3])              # Wypisuje znaki od indeksu 0 do 2, czyli 'Pyt'
+print(course[0:])               # Wypisuje wszystkie znaki od indeksu 0 do końca, czyli 'Python Programming'
+print(course[:3])               # Wypisuje znaki od początku do indeksu 2, czyli 'Pyt'
+print(course[:])                # Wypisuje wszystkie znaki, czyli 'Python Programming'
