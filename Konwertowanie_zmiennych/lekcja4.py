ba2bfb2d1418a9f4beba485f5bedb8812ae03640
Dariusz-Kubicki <kubicki.dariusz22@gmail.com>
2026-05-06 14:09:59 +0200

    Update lekcja4.py

--- Konwertowanie_zmiennych/lekcja4.py
+++ Konwertowanie_zmiennych/lekcja4.py
@@ -1,7 +1,9 @@
+x = input("x: ")  # Pobieramy wartość od użytkownika i przypisujemy ją do zmiennej x.
+y = x + 1         # To spowoduje błąd (TypeError)!
+print(y)
 
-x = input("x : ")                                # Pobieramy wartość od użytkownika i przypisujemy ją do zmiennej x. Jednak input() zwraca string, więc x będzie typu string.
-print(type(x))                                   # To pokaże, że x jest typu <class 'str'>
-# y = x + 1                                        To spowoduje błąd, ponieważ nie można dodać liczby (1) do stringa (x).
+x = input("x: ") 
+print(type(x))    # Wynik: <class 'str'>
 
 # Przykład konwersji typów danych:
 # int(x) - konwertuje x na liczbę całkowitą
@@ -10,15 +12,15 @@ print(type(x))                                   # To pokaże, że x jest typu <
 # str(x) - konwertuje x na string
 
 # Poprawna konwersja z stringa na liczbę całkowitą
-x = input("x : ")
-y = int(x) + 1                                   # Teraz x jest konwertowane na int, więc możemy dodać 1
-print(f"x: {x}, y: {y}")                         # To pokaże poprawny wynik, np. jeśli użytkownik wpisał "5", to y będzie 6
+x = input("x: ")             # Pobieramy tekst (np. "5")
+y = int(x) + 1               # Konwertujemy tekst na liczbę i dodajemy 1
+print(f"x: {x}, y: {y}")     # Wynik: x: 5, y: 6
 
 # Przykład konwersji z stringa na boolean
-print(f"bool('') = {bool('')}")                  # Pusty string jest False
-print(f"bool('Hello') = {bool('Hello')}")        # Niepusty string jest True
-print(f"bool(0) = {bool(0)}")                    # Zero jest False
-print(f"bool(1) = {bool(1)}")                    # Niezero jest True
-print(f"bool([]) = {bool([])}")                  # Pusta lista jest False
-print(f"bool([1, 2, 3]) = {bool([1, 2, 3])}")    # Niepusta lista jest True
-print(f"bool(None) = {bool(None)}")              # None jest False
+# Co zwraca True, a co False?
+print(f"bool('') = {bool('')}")                  # Pusty string -> False
+print(f"bool('Hello') = {bool('Hello')}")        # Niepusty string -> True
+print(f"bool(0) = {bool(0)}")                    # Zero -> False
+print(f"bool(1) = {bool(1)}")                    # Każda inna liczba -> True
+print(f"bool([]) = {bool([])}")                  # Pusta lista -> False
+print(f"bool(None) = {bool(None)}")              # Brak wartości -> False
