c658f34a1550efca3ff81fba65ef7817cc68f330
Dariusz-Kubicki <kubicki.dariusz22@gmail.com>
2026-06-22 13:13:36 +0200

    Zadanie 2.7 z książki

---
+++ Programy_z_ksiazki/Usuniecie_bialych_znakow_z_imienia.py
@@ -0,0 +1,5 @@
+name = "\n\t           Dariusz Kubicki          \t\n\t"
+print(f"Początkowa wersja: {name}")
+print(f'Użycie metody ".lstrip()": {name.lstrip()}')
+print(f'Użycie metody ".rstrip()": {name.rstrip()}')
+print(f'Użycie metody ".strip()": {name.strip()}')
