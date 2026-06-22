cb030389763e42c531601bbebcb4129a42aab0a7
Dariusz-Kubicki <kubicki.dariusz22@gmail.com>
2026-06-22 13:03:38 +0200

    Zadanie 2.5 z książki

---
+++ Programy_z_ksiazki/Slawny_cytat.py
@@ -0,0 +1,2 @@
+message = 'Albert Einstein powiedział kiedyś: "Sformułowanie problemu jest często ważniejsze niż jego rozwiązanie, które może być jedynie kwestią umiejętności matematycznych lub eksperymentalnych"'
+print(message)
