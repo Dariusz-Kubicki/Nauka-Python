7f8698c0e1128eacbe6567971363bf4d3a227c30
Dariusz-Kubicki <kubicki.dariusz22@gmail.com>
2026-06-22 13:07:40 +0200

    Zadanie 2.6 z książki

---
+++ Programy_z_ksiazki/Slawny_cytat_2.py
@@ -0,0 +1,3 @@
+famous_person = "Albert Einstein"
+message = f'"Sformułowanie problemu jest często ważniejsze niż jego rozwiązanie, które może być jedynie kwestią umiejętności matematycznych lub eksperymentalnych" \n-{famous_person}'
+print(message)
